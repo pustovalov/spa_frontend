09bf1fa552c3cb137f19952826f018449a6d478e
pustovalov <pustovalov.pavel@gmail.com>
2016-10-25 02:43:46 +0300

    Redux
    .
    
    Polish

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
+import { Provider, connect } from 'react-redux'
+
 import Post from '../components/Post.jsx'
 import PostForm from '../components/PostForm.jsx'
+
 import 'whatwg-fetch'
 const BASE_URL = process.env.BASE_URL
 
+//action
+import addPost from '../actions/PostActions.js'
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAddPostClick: () => dispatch(addPost)
+  }
+}
+
+function mapStateToProps() {
+  return {
+    value: {}
+  }
+}
+
+class Button extends Component {
+  render() {
+    const { onAddPostClick } = this.props
+    return (
+      <button onClick={onAddPostClick} className="btn btn-default">Test</button>
+    )
+  }
+}
+
+const Test = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Button)
+
+
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
   componentWillMount() {
     this.getData()
   }
@@ -28,6 +64,10 @@ export default class App extends Component {
           return <Post key={post.id} id={post.id} username={post.username} title={post.title} body={post.body} />
         })}
         <PostForm onPostSubmit={this.handlePostSubmit.bind(this)} />
+        <br/>
+        <Provider store={this.props.store}>
+          <Test />
+        </Provider>
       </div>
     )
   }
---
+++ src/reducers/posts.js
@@ -0,0 +1,11 @@
+// Reducer
+function posts(state = {}, action) {
+  switch (action.type) {
+    case 'ADD_POST':
+      console.log("HEY")
+    default:
+      return state
+  }
+}
+
+export default posts
