8fd6973fd96a186be2c01ac7e8883d99a320a98d
pustovalov <pustovalov.pavel@gmail.com>
2017-01-24 01:56:55 +0200

    jest

--- src/components/__tests__/Navbar-test.js
+++ src/components/__tests__/Navbar-test.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar.jsx';
 import createComponentWithIntl from '../../utils/createComponentWithIntl';
 import renderer from 'react-test-renderer';
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-
-const middlewares = [ thunk ]
-const mockStore = configureMockStore(middlewares)
+import mockStore from 'redux-mock-store'
 
 test('Link changes the class when hovered', () => {
   const component = createComponentWithIntl(<Navbar/>);
