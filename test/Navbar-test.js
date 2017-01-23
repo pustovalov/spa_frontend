2870666a337c8fde37994968e9d09e55c2737129
pustovalov <pustovalov.pavel@gmail.com>
2017-01-23 23:31:38 +0200

    specs

--- test/Navbar-test.js
+++ test/Navbar-test.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
-import Foo from '../src/Foo';
+import Navbar from '../src/components/Navbar';
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(shallow(<Foo />).contains(<div className="foo" />)).to.equal(true);
+describe('<Navbar />', () => {
+
+  it('calls componentDidMount', () => {
+    const wrapper = mount(<Navbar />);
+    expect(Navbar.prototype.componentDidMount.calledOnce).to.equal(true);
   });
 
-  it("contains spec with an expectation", function() {
-    expect(shallow(<Foo />).is('.foo')).to.equal(true);
-  });
-
-  it("contains spec with an expectation", function() {
-    expect(mount(<Foo />).find('.foo').length).to.equal(1);
-  });
 });
